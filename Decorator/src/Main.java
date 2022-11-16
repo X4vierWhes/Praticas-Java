d0891b33a4379827a6f4410c264fefdcfa7c3243
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2022-11-16 20:25:38 -0300

    Exemplo basico do padrão Decorator em Java

--- Decorator/src/Main.java
+++ Decorator/src/Main.java
@@ -10,16 +10,6 @@ public class Main  {
 		Lanche lanche = new Lanchonete();
 		lanche = new MistoQuente(new MilkShake(new Pastel(lanche)));
 		System.out.println(lanche.value());
-		
-		/*
-		 * MilkShake mk = new...();
-		 * MistoQuente mq = new...();
-		 * Pastel p = new...();
-		 * 
-		 * System.out.println(mk.value() + mq.value() + p.value());
-		 * 
-		 * 
-		 * */
 	}
 
 }
