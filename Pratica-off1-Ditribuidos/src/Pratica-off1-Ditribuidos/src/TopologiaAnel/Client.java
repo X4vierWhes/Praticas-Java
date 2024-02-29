7841730607c017e860c16e98472732af9c7a3cba
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2024-02-29 17:35:51 -0300

    wip: praticaoff1

---
+++ Pratica-off1-Ditribuidos/src/Pratica-off1-Ditribuidos/src/TopologiaAnel/Client.java
@@ -0,0 +1,47 @@
+package TopologiaAnel;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client{
+
+    public ClientSocket clientSocket;
+
+    public void run(){
+
+        System.out.println("HostAddress = " + clientSocket.getHostAddress());
+        System.out.println("HostName = " + clientSocket.getHostName());
+
+        System.out.println(clientSocket.getLogin() + " " + clientSocket.getIp());
+
+
+        ImplClient c = new ImplClient(clientSocket);
+        Thread t = new Thread(c);
+        t.start();
+
+    }
+
+
+    public Client(String host, int port){
+        String login;
+        try {
+            System.out.println("Login: ");
+            Scanner cin = new Scanner(System.in);
+            login = cin.next();
+            //System.out.println(login);
+            clientSocket = new ClientSocket(new Socket(host, port), login);
+            clientSocket.setIp(host);
+            clientSocket.sendMensage(clientSocket.getLogin()); //Enviando Login para servidor
+            this.run();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        new Client("127.0.0.1", 4001);
+    }
+}
---
+++ Pratica-off1-Ditribuidos/src/Pratica-off1-Ditribuidos/src/TopologiaAnel/Server.java
@@ -0,0 +1,74 @@
+package TopologiaAnel;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+
+public class Server{
+
+    ServerSocket serverSocket;
+    ClientSocket clientSocket;
+
+    int port;
+
+    public Server(int port){
+        this.port = port;
+        this.run();
+    }
+
+
+    public void run() {
+        try {
+            serverSocket = new ServerSocket(this.port); //Iniciando socket do servidor;
+
+            System.out.println("Servidor rodando na porta " +
+                    serverSocket.getLocalPort());
+            System.out.println("HostAddress = " +
+                    InetAddress.getLocalHost().getHostAddress());
+            System.out.println("HostName = " +
+                    InetAddress.getLocalHost().getHostName());
+
+            System.out.println("Aguardando conexão do cliente...");
+            String login;
+
+            this.connectionLoop(); //Entrando em loop ue conectara os clientes
+
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+    }
+
+    private void connectionLoop() throws IOException {
+        //Conexão com cliente
+
+        String msg;
+
+        while(true){
+
+            clientSocket = new ClientSocket(serverSocket.accept());
+            //Extraindo nome de Login;
+            if(((msg = clientSocket.readMessage()) != null) && (clientSocket.getLogin() == null) && clientSocket != null){
+                System.out.print("Conexão " +
+                        (ImplServer.cont++) +
+                        " com o cliente "+
+                        clientSocket.getHostAddress() +
+                        "/" +
+                        clientSocket.getHostName()
+                );
+                clientSocket.setLogin(msg);
+                System.out.print(" Conectado como -> " + clientSocket.getLogin());
+                System.out.println();
+            }
+
+
+            ImplServer server = new ImplServer(clientSocket);
+            Thread t = new Thread(server);
+            t.start();
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server(4000);
+    }
+}
