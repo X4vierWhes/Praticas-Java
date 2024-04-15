8af317ffed007008262527da849b2c53bdb0dd89
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2024-04-15 11:14:40 -0300

    Update Arquivo.java

--- ED2/Trabalho3_Ed2/src/Dao/Arquivo.java
+++ ED2/Trabalho3_Ed2/src/Dao/Arquivo.java
@@ -21,7 +21,7 @@ public class Arquivo {
 
 	
 	@SuppressWarnings("unused")
-	public static void addLog(Veiculo veiculo) {
+	public static void logAdd(Veiculo veiculo) {
         String nomeArquivo = "src/Resources/log_ADD.txt";
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo, true));
